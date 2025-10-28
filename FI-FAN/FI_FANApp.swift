d8b85944b61291d76da395b1d3c1b5a75f944eb9
Israel Tiburcio <agenor.it@gmail.com>
2025-10-27 20:48:40 -0600

    Initial Commit

---
+++ FI-FAN/FI_FANApp.swift
@@ -0,0 +1,17 @@
+//
+//  FI_FANApp.swift
+//  FI-FAN
+//
+//  Created by Israel Tiburcio Suchil on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FI_FANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FI-FANTests/FI_FANTests.swift
@@ -0,0 +1,17 @@
+//
+//  FI_FANTests.swift
+//  FI-FANTests
+//
+//  Created by Israel Tiburcio Suchil on 27/10/25.
+//
+
+import Testing
+@testable import FI_FAN
+
+struct FI_FANTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
